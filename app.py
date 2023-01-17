13a293e4f37767ec3e94d2d6d37249f5c29b96d0
michalmlk <michal.melka1@gmail.com>
2023-01-17 23:44:17 +0100

    first implementation without export

--- app.py
+++ app.py
@@ -1,16 +1,123 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showerror
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class NegativeValueException(Exception):
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# root window
+root = tk.Tk()
+root.title('Calories calc')
+root.geometry('540x360')
+root.configure(padx=20, pady=10)
+root.resizable(False, False)
+
+
+def BMR(s, w, a, h):
+    if s == 'female':
+        return 655.1 + (9.567 * w) + (1.85 * h) + (-4.68 * a)
+    else:
+        return 66.47 + (13.7 * w) + (5 * h) + (-6.76 * a)
+
+def BMI(w,h):
+    return w/(h/100)**2
+
+
+# frame
+frame = ttk.Frame(root)
+frame.grid(column=0, row=0, sticky="nsew")
+
+# field options
+options = {'padx': 5, 'pady': 5}
+
+#labels and inputs
+sex_label = ttk.Label(frame, text='Płeć')
+sex_label.grid(column=0, row=0, sticky='ew')
+
+sex_input = tk.StringVar()
+sex_input = ttk.OptionMenu(frame, sex_input, "female", "male")
+sex_input.grid(column=0, row=1, **options)
+
+weight_label = ttk.Label(frame, text='Waga (kg)')
+weight_label.grid(column=0, row=2, sticky='W', **options)
+
+weight_input = tk.DoubleVar()
+weight_input = ttk.Entry(frame, textvariable=weight_input)
+weight_input.grid(column=0, row=3, **options)
+
+age_label = ttk.Label(frame, text='Wiek (lata)')
+age_label.grid(column=0, row=4, sticky='W', **options)
+
+age_input = tk.IntVar()
+age_input = ttk.Entry(frame, textvariable=age_input)
+age_input.grid(column=0, row=5, **options)
+
+height_label = ttk.Label(frame, text='Wzrost (cm)')
+height_label.grid(column=0, row=6, sticky='W', **options)
+
+height_input = tk.IntVar()
+height_input = ttk.Entry(frame, textvariable=height_input)
+height_input.grid(column=0, row=7, **options)
+
+#actions
+
+def action_button_clicked():
+    try:
+        weight = float(weight_input.get())
+        height = int(height_input.get())
+        age = int(age_input.get())
+        bmr = round(BMR(sex_input, weight, age, height), 2)
+        bmi = round(BMI(weight, height), 2)
+
+        if weight <= 0 or height <= 0 or age <= 0:
+            raise NegativeValueException
+
+    except ValueError:
+        showerror(title='Error', message="Błędne dane")
+    except NegativeValueException:
+        showerror(title='Error', message="Wielkości fizyczne nie mogą być ujemne!")
+    else:
+        info_header.config(text="Oto Twoje dane:")
+
+        bmr_val = f'BMR: {bmr} kcal'
+        bmr_label.config(text=bmr_val)
+
+        weight_info_label.config(text=f'Waga: {weight_input.get()} kg')
+        age_info_label.config(text=f'Wiek: {age_input.get()} lat')
+        height_info_label.config(text=f'Wzrost: {height_input.get()} cm')
+
+        bmi_val = f'BMI: {bmi}'
+        bmi_label.config(text=bmi_val)
+
+        weight_input.delete(0, len(weight_input.get()))
+        height_input.delete(0, len(height_input.get()))
+        age_input.delete(0, len(age_input.get()))
+
+#action_button
+action_button = ttk.Button(frame, text='Policz')
+action_button.grid(column=0, row=8, sticky='ew', **options)
+action_button.configure(command=action_button_clicked)
+
+# result label
+info_header =ttk.Label(frame)
+info_header.grid(row=0, column=1, **options)
+
+weight_info_label = ttk.Label(frame)
+weight_info_label.grid(row=1, column=1, **options)
+height_info_label = ttk.Label(frame)
+height_info_label.grid(row=2, column=1, **options)
+age_info_label = ttk.Label(frame)
+age_info_label.grid(row=3, column=1, **options)
+
+bmr_label = ttk.Label(frame)
+bmr_label.grid(row=4, column=1, **options)
+bmi_label = ttk.Label(frame)
+bmi_label.grid(row=5, column=1, **options)
+
+# add padding to the frame and show it
+frame.grid(padx=10, pady=10)
+
+# start the app
+root.mainloop()
